bfa72bf5aea7d328ec2bf28bf87aebcdfbd3721f
zeronf610 <87525113+zeronf610@users.noreply.github.com>
2022-03-01 22:13:03 +0000

    Update ChessEngine.py

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -22,53 +22,90 @@ class GameState():
             self.moveLog.append(move)   #adds the move to the movelog
             self.whiteToMove = not self.whiteToMove   #switches the turn to black
 
-class Pieces():
-    def __init__(self, colour, piece, position):
-        pieces = ['wP', 'wR', 'wN', 'wQ', 'wK', 'wB', 'bP', 'bR', 'bN', 'bQ', 'bK', 'bB']
-        for piece in pieces:
-            if piece[0] == "w":
-                self.colour = "white"
-            elif piece[0] == "b":
-                self.colour = "black"
-
-        for piece in pieces:
-            if piece[1] == "P":
-                self.piece = "pawn"
-            elif piece[1] == "R":
-                self.piece = "rook"
-            elif piece[1] == "N":
-                self.piece = "knight"
-            elif piece[1] == "B":
-                self.piece = "bishop"
-            elif piece[1] == "Q":
-                self.piece = "queen"
-            elif piece[1] == "K":
-                self.piece = "King"
-
-class Pawn(self, x, y, color)
-    def __init__(self, x, y, color):
-    pawns = ['wP', 'bP']
-    for pawn in pawns:
-        if pawn[0] == "w":
-            self.color = "white"
-        elif pawn[1] == "b":
-            self.color = "black"
-
-    if self.color == "white":
-        
 
 
+#class Pieces():
+    #def __init__(self, colour, piece, position):
+       # pieces = ['wP', 'wR', 'wN', 'wQ', 'wK', 'wB', 'bP', 'bR', 'bN', 'bQ', 'bK', 'bB']
+        #for piece in pieces:
+           # if piece[0] == "w":
+              #  self.colour = "white"
+           # elif piece[0] == "b":
+               # self.colour = "black"
+
+        #for piece in pieces:
+           # if piece[1] == "P":
+               # self.piece = "pawn"
+            #elif piece[1] == "R":
+               # self.piece = "rook"
+           # elif piece[1] == "N":
+            #    self.piece = "knight"
+           # elif piece[1] == "B":
+            #    self.piece = "bishop"
+           # elif piece[1] == "Q":
+           #     self.piece = "queen"
+           # elif piece[1] == "K":
+          #      self.piece = "King"
+board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+class Pawn:
+    def __init__(self, x, y, colour):
+        self.row = y
+        self.col = x
+        self.colour = colour #attributes
+
+    def validMove(self, x, y):
+        if self.colour == "white": #if it is a white pawn
+            if x == self.x and y-self.y == 1: #check if remains in same column and moves up 1 (y-self.y as indexes from 0)
+                return True
+            elif self.y == 6 and x == self.x and y-self.y == 2: #check if it is in starting pos, so it can move 2
+                return True
+            elif board[self.y+1][self.x+1] != "--" and abs(self.x-x) == 1 and y-self.y == 1:
+                return True
+            else:
+                return False
 
 
 
-#class Rook(x,y):
-#class Bishop(x,y):
-#class Knight(x,y):
-#class Queen(x,y):
-#class King(x,y):
 
 
-#wP = Pawn(x, y)
+
+
+
+
+
+class Rook(x,y):
+
+class Bishop(x,y):
+
+class Knight(x,y):
+
+class Queen(x,y):
+
+class King(x,y):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+wP = Pawn(x, y)
 
 class Move():
 
